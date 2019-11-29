706e604a3f3aee3a35af30bd819f431ce8941696
Philipp Meyer <p.j.meyer@tum.de>
2019-11-29 14:22:53 +0100

    Add solution for 2018 day 2 part 2

--- 2018/src/day2.rs
+++ 2018/src/day2.rs
@@ -7,7 +7,7 @@ fn main() {
     let boxes: Vec<String> = common::get_lines();
     let mut two = 0;
     let mut three = 0;
-    for x in boxes {
+    for x in &boxes {
         let mut letters = HashMap::with_capacity(x.len());
 
         for c in x.chars() {
@@ -19,4 +19,21 @@ fn main() {
     }
     let result = two * three;
     println!("Checksum: {}", result);
+
+    for (i, x) in boxes.iter().enumerate() {
+        for y in boxes.iter().skip(i + 1) {
+            let mut diffs = 0;
+            let mut common_letters: String = String::with_capacity(x.len());
+            for (c, d) in x.chars().zip(y.chars()) {
+                if c == d {
+                    common_letters.push(c);
+                } else {
+                    diffs += 1;
+                }
+            }
+            if diffs == 1 {
+                println!("Common letters: {}", common_letters);
+            }
+        }
+    }
 }
