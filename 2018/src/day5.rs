6ccd30b904e62b233f22f52f923b9bab5faf7fba
Philipp Meyer <p.j.meyer@tum.de>
2019-11-30 11:43:24 +0100

    Add solution for 2018 day 5 part 2

--- 2018/src/day5.rs
+++ 2018/src/day5.rs
@@ -5,31 +5,41 @@ fn react(polymer: &str) -> String {
     let mut s = String::from(polymer);
     loop {
         // add end of polymer character
-        s.push('#');
         let mut t = String::new();
         let mut skip = false;
-        for (c, d) in s.chars().zip(s.chars().skip(1)) {
+        for (c, d) in s
+            .chars()
+            .zip(s.chars().skip(1).chain(vec!['#'].into_iter()))
+        {
             if skip {
                 skip = false;
                 continue;
             }
             if c != d && c.to_uppercase().next() == d.to_uppercase().next() {
                 skip = true;
-            }
-            else {
+            } else {
                 t.push(c)
             }
         }
-        // remove end of polymer character again
-        s.pop();
         if t == s {
             break;
-        }
-        else {
+        } else {
             s = t;
         }
     }
     s
+}
+
+fn shortest_with_deletion(polymer: &str) -> String {
+    let reacted = (b'A'..=b'Z').map(|b| {
+        let c = b as char;
+        let s: String = polymer
+            .chars()
+            .filter(|d| d.to_uppercase().next() != Some(c))
+            .collect();
+        react(&s)
+    });
+    reacted.min_by_key(|s| s.len()).unwrap()
 }
 
 #[allow(dead_code)]
@@ -37,8 +47,9 @@ fn main() {
     let input: Vec<String> = common::get_lines();
 
     for polymer in input {
-        let result = react(&polymer);
-
-        println!("Polymer after reaction has size: {}", result.len());
+        let result1 = react(&polymer);
+        println!("Polymer after reaction has size: {}", result1.len());
+        let result2 = shortest_with_deletion(&polymer);
+        println!("Shortest polymer after deletion and reaction has size: {}", result2.len());
     }
 }
