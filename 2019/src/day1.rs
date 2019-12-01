cfea8b68c206908d9371c4c66c9689794637c9f7
Philipp Meyer <p.j.meyer@tum.de>
2019-12-01 10:38:10 +0100

    Add solution for 2019 day 1 part 2

--- 2019/src/day1.rs
+++ 2019/src/day1.rs
@@ -1,12 +1,29 @@
 #[path = "common.rs"]
 mod common;
 
+fn simple_fuel_for_mass(m: i32) -> i32 {
+    std::cmp::max(m / 3 - 2, 0)
+}
+
+fn fuel_for_mass(m: i32) -> i32 {
+    let mut mass = m;
+    let mut fuel = 0;
+    while mass > 0 {
+        let f = simple_fuel_for_mass(mass);
+        fuel += f;
+        mass = f;
+    }
+    fuel
+}
+
 #[allow(dead_code)]
 fn main() {
-    let result: i32 = common::get_lines()
+    let input: Vec<i32> = common::get_lines()
         .into_iter()
         .map(|l| l.parse::<i32>().expect("could not parse number"))
-        .map(|n| n / 3 - 2)
-        .sum();
-    println!("Total fuel required: {}", result);
+        .collect();
+    let result1: i32 = input.iter().map(|&m| simple_fuel_for_mass(m)).sum();
+    let result2: i32 = input.iter().map(|&m| fuel_for_mass(m)).sum();
+    println!("Part1: Total fuel required: {}", result1);
+    println!("Part2: Total fuel required: {}", result2);
 }
