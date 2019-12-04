e78648033528085481c3a72264902bdf03e8146f
Philipp Meyer <p.j.meyer@tum.de>
2019-12-04 09:17:24 +0100

    Improve performance for 2019 day 4 part 2

--- 2019/src/day4.rs
+++ 2019/src/day4.rs
@@ -35,11 +35,38 @@ fn check_criteria_part1(n: i64) -> bool {
     has_double
 }
 
+#[allow(dead_code)]
 fn check_criteria_part2(n: i64) -> bool {
     let digits = get_digits(n);
     let has_double = digits.iter().group_by(|&d| d).into_iter().any(|(_, e)| e.count() == 2);
     let increasing = digits.iter().fold((true, -1), |(inc, c), &d| (inc && c <= d, d)).0;
     has_double && increasing
+}
+
+fn check_criteria_part2_fast(n: i64) -> bool {
+    let digits = get_digits(n);
+    let mut last: i8 = -1;
+    let mut digit_count = 1;
+    let mut has_double = false;
+    for &d in &digits {
+        if d < last {
+            return false;
+        }
+        if d == last {
+            digit_count += 1;
+        }
+        else {
+            if digit_count == 2 {
+                has_double = true;
+            }
+            digit_count = 1;
+        }
+        last = d;
+    }
+    if digit_count == 2 {
+        has_double = true;
+    }
+    has_double
 }
 
 fn count_passwords<F>(start: i64, end: i64, criterion: F) -> usize
@@ -69,7 +96,7 @@ fn main() {
             start, end, result1
         );
 
-        let result2 = count_passwords(start, end, check_criteria_part2);
+        let result2 = count_passwords(start, end, check_criteria_part2_fast);
         println!(
             "Part2: Number of matching passwords in range {}-{}: {}",
             start, end, result2
@@ -100,5 +127,12 @@ mod tests {
         assert!(check_criteria_part2(112_233));
         assert!(!check_criteria_part2(123_444));
         assert!(check_criteria_part2(111_122));
+    }
+
+    #[test]
+    fn test_numbers_part2_fast() {
+        assert!(check_criteria_part2_fast(112_233));
+        assert!(!check_criteria_part2_fast(123_444));
+        assert!(check_criteria_part2_fast(111_122));
     }
 }
