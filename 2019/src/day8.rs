78b0e14708845c3ad2e4cd8d2d2a2dc0677e5c5d
Philipp Meyer <p.j.meyer@tum.de>
2019-12-08 14:02:27 +0100

    Add solution for 2019 day 8 part 2

--- 2019/src/day8.rs
+++ 2019/src/day8.rs
@@ -1,6 +1,8 @@
 mod common;
+use ansi_term::Colour::{Black, Red, White};
 use generic_array::typenum::{U25, U6};
 use generic_array::{ArrayLength, GenericArray};
+use std::fmt;
 use std::str::FromStr;
 
 #[derive(Debug)]
@@ -11,6 +13,29 @@ struct Layer<Width: ArrayLength<u8>, Height: ArrayLength<GenericArray<u8, Width>
 #[derive(Debug)]
 struct Image<Width: ArrayLength<u8>, Height: ArrayLength<GenericArray<u8, Width>>> {
     layers: Vec<Layer<Width, Height>>,
+}
+
+impl<Width: ArrayLength<u8>, Height: ArrayLength<GenericArray<u8, Width>>> fmt::Display
+    for Layer<Width, Height>
+{
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        let width = Width::to_usize();
+        let height = Height::to_usize();
+        for y in 0..height {
+            let row = &self.pixels[y];
+            for x in 0..width {
+                let p = row[x];
+                let out = match p {
+                    0 => White.on(Black).paint(" "),
+                    1 => Black.on(White).paint(" "),
+                    _ => Red.paint(format!("{}", x)),
+                };
+                write!(fmt, "{}", out)?;
+            }
+            writeln!(fmt)?;
+        }
+        Ok(())
+    }
 }
 
 impl<Width: ArrayLength<u8>, Height: ArrayLength<GenericArray<u8, Width>>> FromStr
@@ -38,12 +63,36 @@ impl<Width: ArrayLength<u8>, Height: ArrayLength<GenericArray<u8, Width>>> FromS
 }
 
 impl<Width: ArrayLength<u8>, Height: ArrayLength<GenericArray<u8, Width>>> Layer<Width, Height> {
+    fn transparent() -> Layer<Width, Height> {
+        let twos = vec![vec![2; Width::to_usize()]; Height::to_usize()];
+        Layer {
+            pixels: GenericArray::from_exact_iter(
+                twos.iter()
+                    .map(|r| GenericArray::clone_from_slice(r.as_slice())),
+            )
+            .unwrap(),
+        }
+    }
+
     fn count_digits(&self, d: u8) -> usize {
         self.pixels
             .iter()
             .flat_map(|r| r.iter())
             .filter(|&&p| p == d)
             .count()
+    }
+
+    fn add(&mut self, other: &Layer<Width, Height>) {
+        let width = Width::to_usize();
+        let height = Height::to_usize();
+        for y in 0..height {
+            for x in 0..width {
+                let p = self.pixels[y][x];
+                let q = other.pixels[y][x];
+                let r = if p == 2 { q } else { p };
+                self.pixels[y][x] = r;
+            }
+        }
     }
 }
 
@@ -71,10 +120,24 @@ impl<Width: ArrayLength<u8>, Height: ArrayLength<GenericArray<u8, Width>>> FromS
 
 impl<Width: ArrayLength<u8>, Height: ArrayLength<GenericArray<u8, Width>>> Image<Width, Height> {
     fn checksum(&self) -> usize {
-        let min_zero_layer = self.layers.iter().map(|l| (l, l.count_digits(0))).min_by_key(|&(_, zeros)| zeros).unwrap().0;
+        let min_zero_layer = self
+            .layers
+            .iter()
+            .map(|l| (l, l.count_digits(0)))
+            .min_by_key(|&(_, zeros)| zeros)
+            .unwrap()
+            .0;
         let ones = min_zero_layer.count_digits(1);
         let twos = min_zero_layer.count_digits(2);
         ones * twos
+    }
+
+    fn decode(&self) -> Layer<Width, Height> {
+        let mut layer = Layer::transparent();
+        for l in &self.layers {
+            layer.add(&l);
+        }
+        layer
     }
 }
 
@@ -83,9 +146,13 @@ fn main() {
         .into_iter()
         .map(|l| l.parse::<Image<U25, U6>>().expect("could not parse image"))
         .collect();
+    
     for image in input {
         let result1 = image.checksum();
         println!("Part1: image checksum: {}", result1);
+
+        let decoded = image.decode();
+        println!("Part2 decoded image:\n{}", decoded);
     }
 }
 
@@ -107,5 +174,17 @@ mod tests {
 
         let checksum = image.checksum();
         assert_eq!(checksum, 1);
+    }
+
+    #[test]
+    fn test_image_decode() {
+        use generic_array::typenum::U2;
+        let input = "0222112222120000";
+        let image = input
+            .parse::<Image<U2, U2>>()
+            .expect("could not parse image");
+        let decoded = image.decode();
+        assert_eq!(decoded.pixels[0].as_slice(), [0, 1]);
+        assert_eq!(decoded.pixels[1].as_slice(), [1, 0]);
     }
 }
